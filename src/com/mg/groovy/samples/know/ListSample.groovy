099a9d74a8e7d943c5441941fe1388b1d0d32f8a
Marcio Garcia <marcio@mangar.com.br>
2009-02-21 17:55:47 +1100

    finishing lists....

--- src/com/mg/groovy/samples/know/ListSample.groovy
+++ src/com/mg/groovy/samples/know/ListSample.groovy
@@ -105,8 +105,171 @@ public class ListSample{
     }
 	
     @Test
-    public void test_A() {
+    public void test_Remove() {
 	
+		def list = ['a','b','c']
+		
+		//by position...
+        list.remove(2)    
+        assert list == ['a','b']
+		
+		//by value....
+        list.remove('b')    
+    
+        list = ['a','b','b','c']
+		
+		//by range...
+        list.removeAll(['b','c'])                
+        assert list == ['a']   	
+	
+	
+    }
+    
+    @Test
+    public void test_Methods() {
+
+		//flatten...
+		assert [1,[2,3]].flatten() == [1,2,3]       
+	        
+		//intersect...
+		assert [1,2,3].intersect([4,3,1])== [3,1]
+		
+		//disjoint....
+		assert [1,2,3].disjoint([4,5,6])            
+		                                              
+		def list = [1,2,3]                              
+		def popped = list.pop()   
+		assert popped == 3                          
+		assert list == [1,2]                        
+		                   
+		//reverse....
+		assert [1,2].reverse() == [2,1]             
+		             
+		//sort....
+		assert [3,1,2].sort() == [1,2,3]            
+		                                                                 
+		list = [ [1,0], [0,1,2] ]      
+		list = list.sort { a,b -> a[0] <=> b[0] }   
+		assert list == [ [0,1,2], [1,0] ]
+		
+		list = list.sort { item -> item.size() }   
+		assert list == [ [1,0], [0,1,2] ] 
+		                                              
+    }
+       
+    @Test
+    public void test_FindAllCollect() {
+        //collect returns all element inside the closure...                                 
+        def doubled = [1,2,3].collect{ item ->   
+            item*2                                    
+        }                                             
+        assert doubled == [2,4,6]                      
+                                                      
+        //... return only the elements true inside the closure.
+        def odd = [1,2,3].findAll{ item ->   
+            item % 2 == 1 
+        } 
+        assert odd == [1,3]	
+    }
+    
+    @Test
+    public void test_RemoveDuplicate() {
+	
+		println "test_RemoveDuplicate - "*5
+		def x = [1,1,1] 
+		println x
+		
+		//way 1
+		assert [1] == new HashSet(x).toList()
+		println new HashSet(x).toList()
+		
+		//way 2 (best)
+		assert [1] == x.unique()
+		println x.unique()
+    }  
+    
+    @Test
+    public void test_RemoveNull() {
+	
+		println "test_RemoveNull - "*5
+	
+		def x = [1,null,1] 
+		println x
+		
+		assert [1,1] == x.findAll{it != null}
+		println x.findAll{it != null}
+		
+		assert [1,1] == x.grep{it}
+		println x.grep{it}
+    }
+
+    @Test
+    public void test_Query() {
+		println "test_Query - "*5
+		
+		def list = [1,2,3] 
+		assert list.count(2) == 1               
+		assert list.max() == 3                  
+		assert list.min() == 1                  
+		              
+		//only who closure returns true...
+		def even = list.find { item ->          
+		    item % 2 == 0                       
+		}                                       
+		assert even == 2                        
+		                 
+		//everyone who closure returns true...
+		assert list.every { item -> item < 5} 
+		
+		//anyone who clojure returns true
+		assert list.any   { item -> item < 2} 
+    }
+    
+    @Test
+    public void test_Iteration() {
+		println "test_Iteration - " * 5
+		
+		def store = '' 
+		def list = [1, 2, 3]
+		println list
+		
+		//all elements...
+	    list.each { item ->         
+	        store += item           
+	    }                           
+	    assert store == '123'  
+	    println store
+	                                
+	    store = ''
+	    //all elements inversed.....
+	    list.reverseEach{ item ->   
+	        store += item           
+	    }                           
+	    assert store == '321'
+	    println store
+
+    }
+
+    @Test
+    public void test_Acumulation() {
+		println "test_Acumulation - " * 5
+		
+		def list = [1,2,3]
+		
+		assert list.join('-') == '1-2-3'
+		println list.join('-')
+            
+	    def result = list.inject(0){ clinks, guests ->   
+	        clinks += guests                         
+	    }                                            
+	    assert result == 0 + 1+2+3
+	    
+	    assert list.sum() == 6                       
+	                                                 
+	    def factorial = list.inject(1){ fac, item ->     
+	        fac *= item                              
+	    }                                            
+	    assert factorial == 1 * 1*2*3	
     }
     
     
