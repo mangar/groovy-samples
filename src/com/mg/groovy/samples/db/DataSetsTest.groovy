77cc32bce8871b47185f84c3e40ce82f7e9ceaca
Marcio Garcia <marcio@mangar.com.br>
2009-03-14 16:02:35 +1100

    ignore file and DataBase connection

---
+++ src/com/mg/groovy/samples/db/DataSetsTest.groovy
@@ -0,0 +1,23 @@
+package com.mg.groovy.samples.db
+
+import org.junit.Test
+
+
+/**
+ * DataSetsTest.groovy
+ *
+ * @author Marcio Garcia - marcio@mangar.com.br
+ */
+public class DataSetsTest{
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	def void test_DataSet01(){
+	    
+	}
+	
+	
+	
+}
